f8078b4e372dd950b96093f1369955d5bc0e3a55
bearstandooo <bearstando@outlook.com>
2025-09-19 10:39:50 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var str = '<div style="position: fixed;z-index: 100000;top: 0;left: 0;opacity: .3;pointer-events: none;height:100%;width:100%;background-color:#ffd500"></div>',
+var str = '<div style="position:fixed;height:100%;width:100%;left:0px;top:0px;opacity: 0.5;"><div style="height:100%;width:100%;position:static;background-color:orange;position: fixed;z-index: 100000;top: 0;left: 0;opacity: .3;pointer-events: none;"></div></div>',
     mainBody = document.getElementsByTagName('body');
-
-mainBody.insertAdjacentHTML('beforeend',str);
+var bodyHTML = mainBody.innerHTML;
+bodyHTML = bodyHTML + str;
