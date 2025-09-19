258d687f8dbc68fe9485294500712dd9ac16c972
bearstandooo <bearstando@outlook.com>
2025-09-19 09:14:20 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+var str = '<div style="position: fixed;z-index: 100000;top: 0;left: 0;opacity: .3;pointer-events: none;height:100%;width:100%;background-color:#ffd500"></div>',
+    mainBody = document.getElementByTagName('body');
+
+mainBody.insertAdjacentHTML('beforeend',str);
